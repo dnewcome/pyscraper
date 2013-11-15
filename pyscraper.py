9a1b643d6ca3f1ae9c67f0ac3cbb9df178252061
Dan Newcome <dan@dnuke.com>
2013-11-14 23:07:04 -0800

    working recursive gets against live target

--- pyscraper.py
+++ pyscraper.py
@@ -1,4 +1,5 @@
 import requests
+import time
 import sys
 import json
 from lxml import etree
@@ -6,11 +7,15 @@ from io import StringIO
 from pyjath import PyJath
 
 trace_enabled = True 
+delay = 5
+
 def trace(msg, arg): 
     if trace_enabled:
         print msg, arg
 
 config = json.loads(sys.stdin.read())
+base = config['base'] if 'base' in config else ''
+
 trace("main config: ", config)
 
 def scrape(config, html):
@@ -25,14 +30,21 @@ def scrape(config, html):
     trace("urls: ", urls)
     trace("template: ", config['template'])
 
+    ret = []
+
     for url in urls:
         trace(url, None)
-        r = requests.get(url['url'])
+        r = requests.get(base + url['url'])
+        print r
         tree = etree.parse(StringIO(r.text), etree.HTMLParser())
         result = PyJath().parse(config['template'], tree)
         trace("result: ", result)
+        ret.append(result)
+        time.sleep(1)
 
         if "config" in config:
-            return scrape(config['config'], tree)
+            ret.append(scrape(config['config'], tree))
 
-scrape(config, None)
+    return ret
+
+print scrape(config, None)
---
+++ test5.sh
@@ -0,0 +1,1 @@
+cat template5 | bin/python pyscraper.py
